c02a851cfafb592322314b495791f03c90432e20
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2023-01-28 18:12:07 -0600

    UploadProject

---
+++ PortfolioDinoUnity/Assets/Dino/Code/InteractiveObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class InteractiveObject : MonoBehaviour
+{
+
+   [GUIColor(0.3f, 0.8f, 0.8f, 1f)]
+   [AssetsOnly]
+   public Material MaterialAsset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
