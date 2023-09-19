615fe59c4474e629c9777880170508ab57eff99b
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2023-09-18 18:00:34 -0600

    upload project and cars movement

---
+++ PortfolioDinoUnity/Assets/Dino/Code/Interactions/Pin.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    void DoTweener()
+    {
+         
+    }
+}
